c048889308f552817addb3d44dfa4ade02faf8f7
Dhiraj Khadka <dhirajkhadka319@gmial.com>
2021-12-02 12:50:40 +1100

    first commit

---
+++ store.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    css
+    /*-home section ---*/
+    .home-section, .full-screen{
+    background-color: blue;
+    padding: 50px 0;
+    min-height :100vh;
+    }
+    .home-text,.home-img{
+      flex:0 0 50%;
+      max-width:50%;
+      padding: 0 15px;
+
+    }
+    .home-text{
+      background-color: violet;
+
+    }
+    .home-img{
+      background-color: red;
+
+    }
+    html
+    <!--home start-->
+
+    <section class "home -section section">
+      <div class="container">
+        <div class="row  full-screen .align-items-center">
+          <div class="home-text">
+            <p>
+              Hello
+            </p>
+            <h2>I'm Dhirj Khadka  </h2>
+          </div>
+          <div class="home-img">
+            <div class="img-box">
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    /*-Button------*/
+    <button class="resume" type="button">Resume</button>
+    <button class="skill" type="button">Skill</button>
+    <button class="art" type="button">Art</button><br />
+    <p>Or Wanna play
+css
+.resume, .skill, .art, .game {
+  background-color: var(--bc1);
+  font-size: 30px;
+  text-align: center;
+  cursor: pointer;
+  font-family: cursive;
+  color: white;
+  border-radius: 10px;
+}
+.projListName {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.projectDesc {
+  font-size: 15px;
+  font-family: cursive;
+  font-weight: lighter;
+  letter-spacing: 1px;
+}
+.about-info-pp{
+  font-size: 16px;
+  line-height: 26px;
+  margin-bottom: 15px;
+    </p>
+    <button class="game" type="button">Game</button>
+  </div>
+  <button class="resume" type="button">Resume</button>
+  <button class="skill" type="button">Skill</button>
+  <button class="art" type="button">Art</button><br />
+  <p>Or Wanna play
+
+  </p>
+  <button class="game" type="button">Game</button>
+</div>
+
+  </body>
+</html>
